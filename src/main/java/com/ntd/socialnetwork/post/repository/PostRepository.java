9a164e1b424cb366dbb0f078b020d0b4926bfe71
Nguyen Tien Dat <tie.dat2004@gmail.com>
2025-06-16 16:24:34 +0700

    add: post feature

--- src/main/java/com/ntd/socialnetwork/post/repository/PostRepository.java
+++ src/main/java/com/ntd/socialnetwork/post/repository/PostRepository.java
@@ -1,12 +1,14 @@
 package com.ntd.socialnetwork.post.repository;
 
 import com.ntd.socialnetwork.post.model.Post;
-import com.ntd.socialnetwork.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
-public interface PostRepository extends JpaRepository<User, Long> {
-    void save(Post post);
+public interface PostRepository extends JpaRepository<Post, Long> {
+    void deleteById(String id);
+    Optional<Post> findById(String id);
 }
